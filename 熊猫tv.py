92703e068da00b453fc64341bc79c9b8ae6b7ad9
836408280@qq.com <836408280@qq.com>
2018-07-30 19:49:40 +0800

    u

--- 熊猫tv.py
+++ 熊猫tv.py
@@ -2,87 +2,47 @@ import re
 import requests
 
 class Spider():
-    """
-    ????tv?????????????
-    """
+    panda_url = "https://www.panda.tv/cate/pubg"
 
-    # ??????url????
-    pubg_url = "https://www.panda.tv/cate/pubg"
-
-    def __response(self):
-        """
-        ??????
-        """
-
-        # ??requests???????
-        response = requests.get(Spider.pubg_url)
-
-        # ??????????
+    def __fetch_content(self):
+        response = requests.get(Spider.panda_url)
         response.encoding = "utf-8"
-        
-        # ????
         htmls = response.text
         return htmls
 
     def __analysis(self, htmls):
-        """
-        ?????html??????????
-        """
-
-        # ????????????????
         rough = re.findall(r'<div class="video-info">([\s\S]*?)</div>', htmls)
-        
-        # anchors??????????
         anchors = []
         for anchor in rough:
-
-            # ????????????????
-            name = re.findall(r'<span class="video-nickname" title="([\s\S]*?)">', anchor)
+            name = re.findall(r'span class="video-nickname" title="(.*?)"', anchor)
             name = str(name[0])
-
-            # ????????????????
-            number = re.findall(r'<span class="video-number">([\s\S]*?)</span>', anchor)
+            number = re.findall(r'<span class="video-number">(.*?)</span>', anchor)
             number = str(number[0])
-            
-            # ????????????????????
             anchor = {'name':name, 'number':number}
             anchors.append(anchor)
         return anchors
-
+    
     def __sort(self, anchors):
-        """
-        ?????????????
-        """
         return sorted(anchors, key=self.__sort_seed, reverse=True)
 
     def __sort_seed(self, anchor):
-        """
-        sorted??????????????????
-        """
-        if '?' in anchor['number']:
+        if '万' in anchor['number']:
             number = int(float(anchor['number'][:-1]) * 10000)
         else:
             number = int(anchor['number'])
         return number
-
+    
     def __show(self, anchors):
-        """
-        ??????
-        """
-        for i in anchors:
-            print('name: ', i['name'],'\n','number:', i['number'],'\n')
+        for rank in range(len(anchors)):
+            print("rank %s: %s       %s" % (rank, anchors[rank]['name'], anchors[rank]['number']))
 
 
 
     def go(self):
-        """
-        Spider???????Spider????????????go?????
-        """
-        htmls = self.__response()
+        htmls = self.__fetch_content()
         rough_list = self.__analysis(htmls)
         sort_list = self.__sort(rough_list)
         return self.__show(sort_list)
 
-
 a = Spider()
-a.go()
+print(a.go())
